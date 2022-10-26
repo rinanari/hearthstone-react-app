cd6d659053e3d79aa620b1d28647d9bf4df64bae
Arina Chernei <nau-arina@yandex.ru>
2022-10-26 16:27:29 +0300

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
