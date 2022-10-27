26fce6222735a4cf1deeab08575c3bf85c9f9edd
Arina Chernei <nau-arina@yandex.ru>
2022-10-27 13:51:57 +0300

    started developing the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import { MainPage } from "./pages/MainPage";
+import { Header } from "./components/Header";
 import "./App.scss";
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,16 @@
+export const MainPage = () => {
+  return (
+    <div>
+      <main className="main-page">
+        <div className="container">
+          <h1>Welcome to our Hearthstone community</h1>
+          <p>
+            This is the place where you can find any information you want about
+            Hearthstone cards.
+          </p>
+        </div>
+        <div className="container">{/* <CardsInfo /> */}</div>
+      </main>
+    </div>
+  );
+};
