3b15525abce6152512b56269cebf2bfb1153b5b4
Arina <73019091+rinanari@users.noreply.github.com>
2022-11-01 11:20:11 +0300

    Update src/hooks/debounce.ts
    Co-authored-by: VictorDanilov <wikslayer@gmail.com>

--- src/hooks/debounce.ts
+++ src/hooks/debounce.ts
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
+
 export function useDebounce(value: string, delay: number = 400) {
   const [debounced, setDebounced] = useState(value);
+  
   useEffect(() => {
     const handler = setTimeout(() => {
       setDebounced(value);
     }, delay);
     return () => clearTimeout(handler);
   }, [value, delay]);
+  
   return debounced;
 }
